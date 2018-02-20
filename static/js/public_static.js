3ae13ba012e7fc13d53b453a11b645db33abbc9f
andyzhaozhao <andy_zhaozhao@hotmail.com>
2018-02-21 00:58:52 +0800

    Add:init project

--- static/js/public_static.js
+++ static/js/public_static.js
@@ -16,10 +16,10 @@
     //定义全局变量
     if (typeof s.static == "undefined") {
         s.static = {
-            //contextPath: "http://182.92.128.240:8889",//后端服务器地址
-            //cmsContextPath: "http://182.92.128.240:8888",//后端服务器地址
-             contextPath: "http://localhost:8890",//后端服务器地址
+            contextPath: "http://localhost:8890",//后端服务器地址
             cmsContextPath: "http://localhost:8890",//后端服务器地址
+            //contextPath: "http://172.247.157.150:8900",//后端服务器地址
+            //cmsContextPath: "http://172.247.157.150:8900",//后端服务器地址
             xauthtoken: "com.jiaxiaoyi.xauthtoken",//sessionid的token
             getURLParameter: function (name) {//获得页面url参数
                 return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
